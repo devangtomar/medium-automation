33df0a76d26fbe6cf31e28b7958f1717c1e22063
devangtomar <devangtomar123@gmail.com>
2021-01-10 00:25:03 +0530

    Initial commit! 🚀

---
+++ index.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+const clientId = 'your-client-id';
+const clientSecret = 'your-client-secret';
+const userId = 'the-user-id-of-the-medium-user';
+
+const options = {
+  url: `https://api.medium.com/v1/users/${userId}/followers`,
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${clientId}:${clientSecret}`,
+  },
+};
+
+request.get(options, (error, response, body) => {
+  if (error) {
+    // Handle error
+  }
+
+  const followers = JSON.parse(body);
+  console.log(followers);
+});
