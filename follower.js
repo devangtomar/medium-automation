a4d8ceacf4dc7cb0a5f5dcb26632460e1928e875
d0t077o <devangtomar123@gmail.com>
2022-12-15 15:05:30 +0530

    Formatting the code

--- follower.js
+++ follower.js
@@ -6,21 +6,19 @@ const ACCESS_TOKEN = "YOUR_ACCESS_TOKEN";
 // Replace with a list of user IDs to follow
 const userIdsToFollow = ["user1", "user2", "user3"];
 
-const client = new medium.MediumClient({
-  clientId: "YOUR_CLIENT_ID",
-  clientSecret: "YOUR_CLIENT_SECRET",
-});
+const client = new medium.MediumClient({clientId: "YOUR_CLIENT_ID", clientSecret: "YOUR_CLIENT_SECRET"});
 
 client.setAccessToken(ACCESS_TOKEN);
 
 (async () => {
-  for (const userId of userIdsToFollow) {
-    try {
-      // Follow the user
-      await client.followUser(userId);
-      console.log(`Successfully followed user: ${userId}`);
-    } catch (error) {
-      console.error(`Error following user ${userId}: ${error.message}`);
+    for (const userId of userIdsToFollow) {
+        try { // Follow the user
+            await client.followUser(userId);
+            console.log(`Successfully followed user: ${userId}`);
+        } catch (error) {
+            console.error(`Error following user ${userId}: ${
+                error.message
+            }`);
+        }
     }
-  }
 })();
