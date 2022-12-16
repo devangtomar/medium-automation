55a2d8943ce1f48fad7ccfe7e418c27d1deb16cd
d0t077o <devangtomar123@gmail.com>
2022-12-16 15:06:34 +0530

    Adding instructions

--- follower.js
+++ follower.js
@@ -1,3 +1,8 @@
+// This script requires medium-sdk, which you can install by running npm install medium-sdk. You will also need to register your app with Medium and obtain a client ID and client secret, which you can do by following the instructions here. You will also need to generate an access token, which you can do by following the instructions here.
+
+// In the script, replace YOUR_ACCESS_TOKEN with the access token you generated, YOUR_CLIENT_ID with your app's client ID, and YOUR_CLIENT_SECRET with your app's client secret. Then, replace the userIdsToFollow array with a list of user IDs that you want to follow. You can find a user's ID by looking at their profile URL on Medium; for example, if their profile URL is https://medium.com/@username, their user ID is username.
+// When you run the script, it will iterate over the list of user IDs and follow each user. If there are any errors, they will be logged to the console.
+
 const medium = require("medium-sdk");
 
 // Replace with your own access token
