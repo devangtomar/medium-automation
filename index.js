67b0af277e4ef3de551f7291a943ff0aa909c4a7
d0t077o <devangtomar123@gmail.com>
2022-12-17 15:08:19 +0530

    Adding formatting to index.js

--- index.js
+++ index.js
@@ -5,18 +5,17 @@ const clientSecret = "your-client-secret";
 const userId = "the-user-id-of-the-medium-user";
 
 const options = {
-  url: `https://api.medium.com/v1/users/${userId}/followers`,
-  headers: {
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${clientId}:${clientSecret}`,
-  },
+    url: `https://api.medium.com/v1/users/${userId}/followers`,
+    headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${clientId}:${clientSecret}`
+    }
 };
 
 request.get(options, (error, response, body) => {
-  if (error) {
-    // Handle error
-  }
+    if (error) { // Handle error
+    }
 
-  const followers = JSON.parse(body);
-  console.log(followers);
+    const followers = JSON.parse(body);
+    console.log(followers);
 });
